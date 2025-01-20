e254c16a3e959a3e64f3df4138473466f1697a5b
cottu <cotugno.davide@itiscannizzaro.edu.it>
2025-01-20 19:03:20 +0100

    Update soapserver.php

--- soapserver.php
+++ soapserver.php
@@ -1,18 +1,13 @@
 <?php
-// Importiamo il file WSDL (Web Service Definition Language)
-$wsdl = 'http://localhost/wsdl/valuta.wsdl'; // Aggiungi il percorso corretto del file WSDL
+$wsdl = 'http://localhost/wsdl/valuta.wsdl'; 
 
-// Funzione per recuperare i tassi di cambio da BCE
 function getExchangeRates($currency) {
     // Recuperiamo il file XML dalla BCE
     $xml = simplexml_load_file("http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml");
 
-    // Verifica che il file XML sia stato caricato correttamente
     if (!$xml) {
         throw new Exception('Impossibile caricare i tassi di cambio');
     }
-
-    // Cicliamo attraverso le valute nel file XML e recuperiamo il tasso per la valuta desiderata
     foreach ($xml->Cube->Cube->Cube as $rate) {
         if ($rate['currency'] == strtoupper($currency)) {
             return (float)$rate['rate'];
@@ -22,7 +17,6 @@ function getExchangeRates($currency) {
     throw new Exception('Valuta non trovata');
 }
 
-// Definiamo la classe del servizio SOAP
 class CurrencyConverter {
     public function convertCurrency($amount, $currency) {
         // Convertiamo il valore passato in Euro (base)
@@ -31,8 +25,8 @@ class CurrencyConverter {
     }
 }
 
-// Creiamo un server SOAP
-$server = new SoapServer($wsdl); // Usa il file WSDL
-$server->setClass('CurrencyConverter'); // Impostiamo la classe che gestirà le richieste
-$server->handle(); // Gestiamo la richiesta SOAP
+// Creo un server SOAP
+$server = new SoapServer($wsdl); 
+$server->setClass('CurrencyConverter'); 
+$server->handle(); 
 ?>

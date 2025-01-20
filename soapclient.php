b8dfb06731e91a1e18316716de63c837bc69d861
cottu <cotugno.davide@itiscannizzaro.edu.it>
2025-01-20 19:02:07 +0100

    Update soapclient.php

--- soapclient.php
+++ soapclient.php
@@ -1,12 +1,12 @@
 <?php
-// Creiamo un client SOAP
+
 $client = new SoapClient('http://localhost/soapserver.php?wsdl'); 
 
-// Recuperiamo i dati dall'utente (importo e valuta da convertire)
-$amount = 100; // Sostituisci con l'importo da convertire
-$currency = 'USD'; // Sostituisci con la valuta di destinazione, per esempio 'USD'
 
-// Chiamata al metodo del server per effettuare la conversione
+$amount = 100; /
+$currency = 'USD'; 
+
+// Converto
 try {
     $result = $client->convertCurrency($amount, $currency);
     echo "L'importo di $amount EUR è equivalente a $result $currency.";
